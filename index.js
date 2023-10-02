e66cba8cf4809b71b590c79f25ea2b2276e9e53c
NemiMoser <135074383+NemiMoser@users.noreply.github.com>
2023-10-02 16:46:26 -0400

    edited font size index.js

--- index.js
+++ index.js
@@ -18,24 +18,34 @@ function generateSVG({ characters, textColor, shape, shapeColor }) {
     const svgWidth = 300;
     const svgHeight= 200;
 
+    let fontSize = 40;
+    const cx = svgWidth / 2;
+    const cy = svgHeight / 2;
+    const radius = Math.min(svgWidth, svgHeight) / 3;
+
+    shapeElement = `<circle cx="${cx}px" cy="${cy}px" r="${radius}px" fill="${shapeColor}" />`;
+    textElement = `<text x="${cx}px" y="${cy}px" text-anchor="middle" dominant-baseline="middle" fill="${textColor}" font-size="${fontSize}px">${characters}</text>`;
+
+
     if (shape === 'circle') {
         const cx = svgWidth / 2;
         const cy = svgHeight / 2;
         const radius = Math.min(svgWidth, svgHeight) / 3;
+        let fontSize = 40;
 
         shapeElement = `<circle cx="${cx}px" cy="${cy}px" r="${radius}px" fill="${shapeColor}" />`;
-        textElement = `<text x="${cx}px" y="${cy}px" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${characters}</text>`;
+        textElement = `<text x="${cx}px" y="${cy}px" text-anchor="middle" dominant-baseline="middle" fill="${textColor}" font-size="${fontSize}px">${characters}</text>`;
     } else if (shape === 'triangle') {
         const points = `${svgWidth / 2},${svgHeight / 4} ${svgWidth / 4},${(3 * svgHeight) / 4} ${(3 * svgWidth) / 4},${(3 * svgHeight) / 4}`;
         shapeElement = `<polygon points="150,50 100,150 200,150" fill="${shapeColor}"/>`;
-        textElement = `<text x="${svgWidth / 2}px" y="${svgHeight / 2}px" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${characters}</text>`;
+        textElement = `<text x="${svgWidth / 2}px" y="${svgHeight / 2}px" text-anchor="middle" dominant-baseline="middle" fill="${textColor}" font-size="${fontSize}px">${characters}</text>`;
     } else if (shape === 'square') {
         const size = Math.min(svgWidth, svgHeight) / 3;
         const x = (svgWidth - size) / 2;
         const y = (svgHeight - size) / 2;
 
         shapeElement = `<rect x="${x}px" y="${y}px" width="${size}px" height="${size}px" fill="${shapeColor}" />`;
-        textElement = `<text x="${svgWidth / 2}px" y="${svgHeight / 2}px" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${characters}</text>`;
+        textElement = `<text x="${svgWidth / 2}px" y="${svgHeight / 2}px" text-anchor="middle" dominant-baseline="middle" fill="${textColor}" font-size="${fontSize}px">${characters}</text>`;
 }
 
     const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="${svgWidth}" height="${svgHeight}">
